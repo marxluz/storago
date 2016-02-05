69f595c6c59a68605e60e2f6c75d84757763a9fe
Marxluz <breno@pastoraldacrianca.org.br>
2016-02-05 17:08:38 -0200

    Initial commit

---
+++ src/storago.js
@@ -0,0 +1,28 @@
+var storago = {};
+;(function(storago){
+
+   var tables = {};
+
+   var Entry = function(name, prop){
+
+   };
+
+   Entry.prototype.hasMany = function(dep_name, dep_table, parent_table){
+
+   };
+
+   var define = function(name, prop){
+
+      var table = Entry();
+   };
+   storago.define = define;
+
+
+
+
+   var schema = function(callback){
+
+   };
+   storago.syncSchema = schema;
+
+}(storago));
