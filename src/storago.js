832ef2274f94f96cf201ed11d209223914e8ec24
marxluz <marxluz@gmail.com>
2016-02-06 20:06:49 -0200

    add test suite

--- src/storago.js
+++ src/storago.js
@@ -1,24 +1,56 @@
 var storago = {};
 ;(function(storago){
 
-   var tables = {};
+   var Metadata = function(){
+     this.hasMany = {};
+     this.parents = {};
+   };
 
    var Entry = function(name, prop){
 
    };
+   Entry.prototype.id = null;
 
-   Entry.prototype.hasMany = function(dep_name, dep_table, parent_table){
+   Entry.hasMany = function(name, parent_entry, many_name){
+     this._META.parents[name] = parent_entry;
+     if(many_name) parent_entry._META.hasMany[many_name] = this;
 
+     Object.defineProperty(this.prototype, name, {
+       get: function(){ return 'hehe' },
+       set: function(obj){
+         console.log(obj._META.name, parent_entry._META.name);
+         if(obj._META.name == parent_entry._META.name){
+           this[name + '_id'] = obj.id;
+         }else{
+           var msg = "(storago) No permission: object must be of class(" + parent_entry._META.name + ")" ;
+           msg += ", but is the class(" + obj._META.name + ")";
+           throw msg;
+         }
+       }
+     });
+
+     if(many_name){
+        Object.defineProperty(parent_entry.prototype, many_name, {
+          'get': function(){},
+        });
+     }
    };
 
-   var define = function(name, prop){
+   var define = function(name, props){
 
-      var table = Entry();
+     var meta = new Metadata();
+     meta.name = name;
+     meta.props = props;
+
+     var table = function(){};
+     table.hasMany = Entry.hasMany;
+     table._META = meta;
+     table.prototype = new Entry();
+     table.prototype._META = meta;
+
+     return table;
    };
    storago.define = define;
-
-
-
 
    var schema = function(callback){
 
---
+++ tests/spec-mult.js
@@ -0,0 +1,39 @@
+describe('DB test', function(){
+
+  it('is exists', function(){
+      expect(window.openDatabase).toBeDefined();
+      expect(storago).toBeDefined();
+  });
+
+  it('create table', function(){
+
+    var Brand = storago.define('brands', {name: 'text'});
+
+    var Car = storago.define('cars', {type: 'text', color: 'text'});
+    Car.hasMany('brand', Brand, 'cars');
+
+    var Boat = storago.define('boats', {type: 'text', size: 'text'});
+
+    var katamaran = new Boat();
+    katamaran.id = 12;
+
+    //console.log(car._META.parents);
+    var fiat = new Brand();
+    fiat.id = 13;
+
+    var vw = new Brand();
+    vw.id = 15;
+
+    var palio = new Car();
+    palio.brand = fiat;
+
+    console.log(palio);
+    //console.log(palio._META);
+
+
+
+
+    //console.log(boat._META);
+
+  });
+});
